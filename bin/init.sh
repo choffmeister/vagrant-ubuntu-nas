f0ab13ff9168cbd06f802f285cd593b2e2436424
Christian Hoffmeister <mail@choffmeister.de>
2014-11-19 06:57:18 +0100

    Format and mount disks

--- bin/init.sh
+++ bin/init.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+# prepare
+apt-get update
+apt-get install -y aptitude
+
+# format disks
+devs="/dev/sdb /dev/sdc /dev/sdd"
+i=1
+for dev in $devs; do
+  echo -e "d\nn\n\n\n\n\nw\nY\n" | gdisk "${dev}"
+  mkfs.ext4 "${dev}1"
+  mkdir -p "/mnt/data${i}"
+  mount "${dev}1" "/mnt/data${i}"
+
+  i=$((i+1))
+done
+
 exit 0
