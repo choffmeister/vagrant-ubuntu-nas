4aa909bfb38b890f7f0f3cd19a177b30bb800adf
Christian Hoffmeister <mail@choffmeister.de>
2014-11-19 18:15:22 +0100

    Mount disks and add mhddfs disk

--- bin/init.sh
+++ bin/init.sh
@@ -10,10 +10,17 @@ i=1
 for dev in $devs; do
   echo -e "d\nn\n\n\n\n\nw\nY\n" | gdisk "${dev}"
   mkfs.ext4 "${dev}1"
+
+  uuid=$(blkid "${dev}1" | sed 's/.*UUID="\([^"]*\)".*/\1/')
   mkdir -p "/mnt/data${i}"
-  mount "${dev}1" "/mnt/data${i}"
+  echo "UUID=${uuid} /mnt/data${i} ext4 defaults 0 2" >> /etc/fstab
 
   i=$((i+1))
 done
+
+# create merged disk
+aptitude install -y mhddfs
+mkdir -p /mnt/data
+echo "mhddfs#/mnt/data1,/mnt/data2,/mnt/data3 /mnt/data fuse defaults,allow_other,nofail,noauto 0 0" >> /etc/fstab
 
 exit 0
