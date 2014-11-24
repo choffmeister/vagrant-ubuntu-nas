336b834d7eabc390265763b5e609fc4f65a32534
Christian Hoffmeister <mail@choffmeister.de>
2014-11-24 05:48:26 +0100

    Mount home folders on data disk

--- bin/init.sh
+++ bin/init.sh
@@ -15,6 +15,7 @@ for dev in $devs; do
   uuid=$(blkid "${dev}1" | sed 's/.*UUID="\([^"]*\)".*/\1/')
   mkdir -p "/mnt/data${i}"
   echo "UUID=${uuid} /mnt/data${i} ext4 defaults 0 2" >> /etc/fstab
+  mount "/mnt/data${i}"
 
   i=$((i+1))
 done
@@ -23,9 +24,14 @@ done
 aptitude install -y mhddfs
 mkdir -p /mnt/data
 echo "mhddfs#/mnt/data1,/mnt/data2,/mnt/data3 /mnt/data fuse defaults,allow_other,nofail 0 0" >> /etc/fstab
+mount "/mnt/data"
 
-# mounting
-mount -a
+# create homes folder
+mkdir -p /mnt/data/homes
+mv /home/* /mnt/data/homes
+echo "/mnt/data/homes /home none bind 0 0" >> /etc/fstab
+mount "/home"
+
 
 # install plex server
 apt-get install -y avahi-daemon avahi-utils
